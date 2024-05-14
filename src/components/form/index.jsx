fec7b081ecfed839307677f3c0ecf20c492b8633
Serge Mayala <serge.mayala@audacy.com>
2024-05-14 17:19:16 +0200

    creating forms

---
+++ src/components/form/index.jsx
@@ -0,0 +1,86 @@
+import { Button, Divider, FormControl, FormLabel, Grid, ListItem, ListItemText, MenuItem, Select, Stack, TextField, Typography } from '@mui/material'
+import MainCard from 'components/MainCard'
+import React, { useState } from 'react'
+
+const Form = ({ fields, onCancel, onSave }) => {
+    const [formValues, setFormValues] = useState({});
+
+  const handleChange = (fieldName) => (event) => {
+    setFormValues((prevValues) => ({
+      ...prevValues,
+      [fieldName]: event.target.value,
+    }));
+  };
+
+  const handleSave = () => {
+    onSave(formValues);
+  };
+
+  return (
+    <Grid container direction={"row"} columnSpacing={2.75}>
+        <Grid item xs={8} >
+            <MainCard>
+            <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+                <Typography variant="h3">Form</Typography>
+            </Stack>
+
+            <Grid container direction={"row"} columnSpacing={2.75} mt={2} mb={1}>
+            {fields.map((field) => (
+                <Grid item md={6} mb={2} key={field.name}>
+            <FormControl fullWidth>
+                <FormLabel>{field.label}</FormLabel>
+              {field.type === 'select' ? (
+                <Select
+                  value={formValues[field.name] || ''}
+                  onChange={handleChange(field.name)}
+                >
+                  <MenuItem value="">Select {field.label}</MenuItem>
+                  {field.options.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>{option.label}</MenuItem>
+                  ))}
+                </Select>
+              ) : (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  required={field.required}
+                  type={field.type}
+                  value={formValues[field.name] || ''}
+                  onChange={handleChange(field.name)}
+                />
+              )}
+            </FormControl>
+                </Grid>
+            ))}
+            </Grid>
+            
+
+            <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+                <Button variant="outlined" onClick={onCancel}  >Cancel</Button>
+                <Button variant="contained" color="primary" onClick={handleSave}>Save</Button>
+            </Stack>
+            </MainCard>
+        </Grid>
+        <Grid item xs={4}>
+            <MainCard>
+            <Stack direction="row" justifyContent="space-between" alignItems="baseline" sx={{ mb: { xs: -0.5, sm: 0.5 } }}>
+                <Typography variant="h3">Preview</Typography>
+            </Stack>
+            <Stack>
+            {Object.entries(formValues).map(([fieldName, value], index) => (
+                <React.Fragment key={fieldName}>
+                <ListItem disableGutters>
+                    <ListItemText  primary={fieldName} secondary={value} />
+                </ListItem>
+                {index !== Object.entries(formValues).length - 1 &&
+                <Divider  /> }
+                </React.Fragment> 
+            ))}
+            </Stack>
+            </MainCard>
+        </Grid>
+    </Grid>
+  )
+}
+
+export default Form
